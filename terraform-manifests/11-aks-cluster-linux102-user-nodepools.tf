453bc7bf9f5a198b679a951a4e94eb1c9da21883
ags <105402016+agsgd@users.noreply.github.com>
2022-09-29 14:30:40 +0530

    Update 11-aks-cluster-linux102-user-nodepools.tf
    new node is added

--- terraform-manifests/11-aks-cluster-linux102-user-nodepools.tf
+++ terraform-manifests/11-aks-cluster-linux102-user-nodepools.tf
@@ -1,10 +1,10 @@
 # Create Linux Azure AKS Node Pool
-/*
+
 resource "azurerm_kubernetes_cluster_node_pool" "linux102" {
   availability_zones    = [1, 2, 3]
   enable_auto_scaling   = true
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-  max_count             = 3
+  max_count             = 1
   min_count             = 1
   mode                  = "User"
   name                  = "linux102"
@@ -26,4 +26,3 @@ resource "azurerm_kubernetes_cluster_node_pool" "linux102" {
     "ui-app"        = "reactjs-apps" 
   }
 }
-*/
